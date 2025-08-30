2fb419ba1f1de81ef1218c4b5cbc70f5ac8c828c
kuldeeplodi <kuldeeplodi@gmail.com>
2025-08-31 04:56:02 +0530

    project complete

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -6,11 +6,12 @@ import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
 import cors from "cors";
 import {app,server} from "./lib/socket.js"
+import path from "path";
 
 dotenv.config();
 
 const PORT=process.env.PORT || 3000;
-
+const __dirname=path.resolve();
 
 
 app.use(express.json({ limit: '10mb' }));
@@ -23,6 +24,15 @@ app.use(cors({
 
 app.use('/api/auth',authRoutes);
 app.use('/api/message',messageRoutes);
+
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static(path.join(__dirname,'../Frontend/dist')))
+
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../Frontend","dist","index.html"))
+    })
+}
+    
 
 server.listen(PORT,()=>{
     console.log("Server is running on port "+PORT);
